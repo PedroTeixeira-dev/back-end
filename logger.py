8ebe1da7a611ff7c300d42c88ce19bd70708d3c3
Pedro Teixeira <pedro15at@gmail.com>
2024-03-24 16:55:57 -0300

    initial commit

---
+++ logger.py
@@ -0,0 +1,17 @@
+from logging.config import dictConfig
+import logging
+import os
+from config import Config
+
+
+log_path = "log/"
+# Verifica se o diretorio para armexanar os logs não existe
+if not os.path.exists(log_path):
+   # então cria o diretorio
+   os.makedirs(log_path)
+
+
+dictConfig(Config.LOGGING)
+
+
+logger = logging.getLogger(__name__)
